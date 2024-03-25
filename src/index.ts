47c51d63f99d697b5357119d705abae390046457
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-25 14:54:50 +0100

    enable localStorage

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,41 @@
 import { ApolloServer } from 'apollo-server-express';
-import express, { Request, Response } from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import jwt from 'jsonwebtoken';
+import express from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { config } from 'dotenv';
-
-import { connectToMongoDB } from './db.js';
 import typeDefs from './schema.js';
 import resolvers from './resolvers.js';
+import { connectToMongoDB } from './db.js';
 import { UserModel } from './models/User.model.js';
-import { ReqWithUserAndCookies } from './types/argTypes.js';
 
 config();
 
-const ORIGIN = process.env.ORIGIN || 'http://localhost:5173';
+const app = express();
 
-console.log(`ORIGIN: ${ORIGIN}`);
 async function startServer() {
-	const app = express();
-
-	app.use(cookieParser());
-
-	const corsOptions = {
-		origin: ORIGIN,
-		credentials: true,
-	};
-
-	app.use(cors(corsOptions));
-
-	// eslint-disable-next-line @typescript-eslint/no-misused-promises
-	app.use(async (req: ReqWithUserAndCookies, res: Response, next) => {
-		try {
-			// Check if token is present in the cookies
-
-			const { token } = req.cookies as { token?: string };
-
-			if (token) {
-				// Token is present, proceed with decoding and retrieving the current user
-
-				const decodedToken = jwt.verify(token, process.env.JWT_SECRET) as { userId: string };
-				const currentUser = await UserModel.findById(decodedToken.userId);
-				req.currentUser = currentUser;
-			}
-		} catch (error) {
-			console.error('Error verifying JWT token:', error);
-		} finally {
-			next(); // Proceed to the next middleware or route handler
-		}
-	});
-
 	const server = new ApolloServer({
 		typeDefs,
 		resolvers,
-		context: ({ req, res }: { req: Request; res: Response }) => ({ req, res }),
+		context: async ({ req }) => {
+			const token = req.headers.authorization || '';
+			if (!token) {
+				return { req, user: null };
+			}
+
+			try {
+				const decodedToken = jwt.verify(token, process.env.JWT_SECRET) as { userId: string };
+				const currentUser = await UserModel.findById(decodedToken.userId);
+				req.currentUser = currentUser;
+
+				return { req };
+			} catch (error) {
+				console.error('Invalid token:', error);
+				return { req, user: null };
+			}
+		},
 	});
 
 	await server.start();
-	server.applyMiddleware({ app, path: '/graphql', cors: false });
+	server.applyMiddleware({ app, path: '/graphql' });
 
 	await connectToMongoDB();
 
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1066,13 +1066,6 @@ const resolvers: Resolvers = {
 				expiresIn: '1d',
 			});
 
-			res.cookie('token', token, {
-				httpOnly: true,
-				secure: true,
-				expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
-				sameSite: true,
-			});
-
 			return { value: token, isAuthenticated: true };
 		},
 
